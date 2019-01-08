050c9a6fd275041b1aeaab50d72c6182aa149c40
mynameisfury <jonathanweatherall0@gmail.com>
2019-01-08 16:45:12 -0600

    database/context created

---
+++ ProjectProjects/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjectProjects
+{
+    public class ApplicationContext : DbContext
+    {
+        public System.Data.Entity.DbSet<ProjectProjects.Models.Project> Projects { get; set; }
+        public System.Data.Entity.DbSet<ProjectProjects.Models.ProjectType> ProjectTypes { get; set; }
+    }
+}
---
+++ ProjectProjects/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ProjectProjects.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProjectProjects.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProjectProjects.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
