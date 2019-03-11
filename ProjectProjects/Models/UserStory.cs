def87fe39a488952c2110ee5a3c13d28f6e38640
mynameisfury <jonathanweatherall0@gmail.com>
2019-03-11 12:21:54 -0500

    userstory model created

---
+++ ProjectProjects/Models/UserStory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjectProjects.Models
+{
+    public class UserStory
+    {
+        [Key]
+        public int ID { get; set; }
+        public string StoryName { get; set; }
+        public string StoryText { get; set; }
+        [ForeignKey("ProjectID")]
+        public int ProjectID { get; set; }
+    }
+}
