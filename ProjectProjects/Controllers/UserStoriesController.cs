29c42d0d642736d2bd4e0164d476d22bb64df427
mynameisfury <jonathanweatherall0@gmail.com>
2019-03-17 17:21:33 -0500

    controllers added

---
+++ ProjectProjects/Controllers/UserStoriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectProjects.Models;
+
+namespace ProjectProjects.Controllers
+{
+    public class UserStoriesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UserStories
+        public ActionResult Index()
+        {
+            var userStories = db.UserStories.Include(u => u.Project);
+            return View(userStories.ToList());
+        }
+
+        // GET: UserStories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserStory userStory = db.UserStories.Find(id);
+            if (userStory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userStory);
+        }
+
+        // GET: UserStories/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserStoryID = new SelectList(db.Projects, "ID", "ProjectName");
+            return View();
+        }
+
+        // POST: UserStories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserStoryID,StoryName,StoryText,ProjectID")] UserStory userStory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserStories.Add(userStory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserStoryID = new SelectList(db.Projects, "ID", "ProjectName", userStory.UserStoryID);
+            return View(userStory);
+        }
+
+        // GET: UserStories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserStory userStory = db.UserStories.Find(id);
+            if (userStory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserStoryID = new SelectList(db.Projects, "ID", "ProjectName", userStory.UserStoryID);
+            return View(userStory);
+        }
+
+        // POST: UserStories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserStoryID,StoryName,StoryText,ProjectID")] UserStory userStory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userStory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserStoryID = new SelectList(db.Projects, "ID", "ProjectName", userStory.UserStoryID);
+            return View(userStory);
+        }
+
+        // GET: UserStories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserStory userStory = db.UserStories.Find(id);
+            if (userStory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userStory);
+        }
+
+        // POST: UserStories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserStory userStory = db.UserStories.Find(id);
+            db.UserStories.Remove(userStory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
